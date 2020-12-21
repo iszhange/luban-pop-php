f7077792263b4a64ad25d83cb71730d839292239
Ken.Zhang <better.man@aliyun.com>
2020-12-21 19:28:21 +0800

    feat(Request): 新增API
    多多客API
    1、生成多多进宝频道推广

--- src/Requests/PddDdkResourceUrlGenRequest.php
+++ src/Requests/PddDdkResourceUrlGenRequest.php
@@ -2,49 +2,53 @@
 /**
  * 生成多多进宝频道推广
  *
- * @link https://open.pinduoduo.com/#/apidocument/port?portId=pdd.ddk.resource.url.gen
- * @author Ken.Zhang <kenphp@yeah.net>
- * Date: 2019/9/22
+ * @link https://open.pinduoduo.com/application/document/api?id=pdd.ddk.resource.url.gen
+ * @author Zhange <kenphp@yeah.net>
+ * Date: 2020/12/21
  * Time: 21:01
  */
 namespace LuBan\Pop\Requests;
 
+use LuBan\Pop\Exceptions\ParameterException;
 use LuBan\Pop\Interfaces\Request;
+use LuBan\Pop\Libs\RequestParamCheckUtil;
 
 class PddDdkResourceUrlGenRequest implements Request
 {
 
-    /**
-     * 接口
-     *
-     * @var string
-     */
-    public $method = 'pdd.ddk.resource.url.gen';
+    private $custom_parameters; // 自定义参数
 
-    /**
-     * 请求方式
-     *
-     * @var string
-     */
-    public $requestType = 'post';
+    private $generate_qq_app; // 是否生成qq小程序
 
-    private $pid; // 推广位ID
+    private $generate_schema_url; // 是否返回 schema URL
 
     private $generate_we_app; // 是否生成小程序
 
-    private $resource_type; // 频道来源：4-限时秒杀,39997-充值中心, 39998-转链type，39999-电器城，39996-百亿补贴
+    private $pid; // 推广位ID
 
-    private $custom_parameters; // 自定义参数
+    private $resource_type; // 频道来源：4-限时秒杀,39997-充值中心, 39998-转链type，39999-电器城，39996-百亿补贴，40000-领券中心
 
     private $url; // 招商多多客ID
 
     private $apiParams = [];
 
 
-    public function setPid($val)
+    public function setCustomParameters($val)
     {
-        $this->pid = (string)$val;
-        $this->apiParams['pid'] = (string)$val;
+        $this->custom_parameters = (string)$val;
+        $this->apiParams['custom_parameters'] = (string)$val;
+    }
+
+    public function setGenerateQqApp($val)
+    {
+        $this->generate_qq_app = (bool)$val;
+        $this->apiParams['generate_qq_app'] = (bool)$val;
+    }
+
+    public function setGenerateSchemaUrl($val)
+    {
+        $this->generate_schema_url = (bool)$val;
+        $this->apiParams['generate_schema_url'] = (bool)$val;
     }
 
     public function setGenerateWeApp($val)
@@ -53,16 +57,16 @@ class PddDdkResourceUrlGenRequest implements Request
         $this->apiParams['generate_we_app'] = (bool)$val;
     }
 
+    public function setPid($val)
+    {
+        $this->pid = (string)$val;
+        $this->apiParams['pid'] = (string)$val;
+    }
+
     public function setResourceType($val)
     {
         $this->resource_type = (int)$val;
         $this->apiParams['resource_type'] = (int)$val;
-    }
-
-    public function setCustomParameters($val)
-    {
-        $this->custom_parameters = (string)$val;
-        $this->apiParams['custom_parameters'] = (string)$val;
     }
 
     public function setUrl($val)
@@ -71,12 +75,21 @@ class PddDdkResourceUrlGenRequest implements Request
         $this->apiParams['url'] = (string)$val;
     }
 
-    /**
-     * 获取参数
-     */
-    public function getParams()
+    public function getApiMethodName()
+    {
+        return 'pdd.ddk.resource.url.gen';
+    }
+
+    public function getApiParas()
     {
         return $this->apiParams;
     }
 
+    /**
+     * @inheritDoc
+     */
+    public function check()
+    {
+        RequestParamCheckUtil::checkNotNull($this->pid, 'pid');
+    }
 }
